ede9fe0ac309cfc0c08d2060da3408086dc916ce
Vilian P <vilian.popov.2020@uni.strath.ac.uk>
2021-07-29 08:27:29 +0300

    Adding REST to project

---
+++ src/main/java/com/example/TweetFilteredAPI/TweetFilteredApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.TweetFilteredAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetFilteredApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetFilteredApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TweetFilteredAPI/TweetFilteredApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TweetFilteredAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetFilteredApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
