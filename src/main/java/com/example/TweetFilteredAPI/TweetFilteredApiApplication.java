92fe82a61574d443e62124d81b0029faaf79b816
Vilian P <vilian.popov.2020@uni.strath.ac.uk>
2021-07-29 15:53:19 +0300

    Adding GCP Support Dependancy

--- src/main/java/com/example/TweetFilteredAPI/TweetFilteredApiApplication.java
+++ src/main/java/com/example/TweetFilteredAPI/TweetFilteredApiApplication.java
@@ -1,13 +1,13 @@
-package com.example.TweetFilteredAPI;
+package com.example.tweetfilteredAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TweetFilteredApiApplication {
+public class TweetfilteredApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TweetFilteredApiApplication.class, args);
+		SpringApplication.run(TweetfilteredApiApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/TweetFilteredAPI/TweetFilteredApiApplicationTests.java
+++ src/test/java/com/example/TweetFilteredAPI/TweetFilteredApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.TweetFilteredAPI;
+package com.example.tweetfilteredAPI;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TweetFilteredApiApplicationTests {
+class TweetfilteredApiApplicationTests {
 
 	@Test
 	void contextLoads() {
